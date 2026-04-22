c75928162a983877799f49aad5ae1340d02cb09b
Kiran <kirankurhade-hub@users.noreply.github.com>
2026-04-22 17:57:15 +0530

    [Kiran] : Volume Measurement Equality, Conversion, and Addition (Litre, Millilitre, Gallon)

---
+++ src/main/java/com/quantityMeasurementApp/VolumeUnit.java
@@ -0,0 +1,31 @@
+package com.quantityMeasurementApp;
+
+public enum VolumeUnit implements IMeasurable {
+	LITRE(1.0), MILLILITRE(0.001), GALLON(3.78541);
+
+	private final double conversionFactor;
+
+	VolumeUnit(double conversionFactor) {
+		this.conversionFactor = conversionFactor;
+	}
+
+	@Override
+	public double getConversionFactor() {
+		return conversionFactor;
+	}
+
+	@Override
+	public double convertToBaseUnit(double value) {
+		return value * conversionFactor;
+	}
+
+	@Override
+	public double convertFromBaseUnit(double value) {
+		return value / conversionFactor;
+	}
+
+	@Override
+	public String getUnitName() {
+		return this.name();
+	}
+}
