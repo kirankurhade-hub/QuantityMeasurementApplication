55aebb1de42d9f6658e6d8707d30c8f20503cc6a
Kiran <kirankurhade-hub@users.noreply.github.com>
2026-04-22 12:11:15 +0530

    [Kiran] : remove unnecessary files and impliments DRY principal.

---
+++ src/main/java/com/quantityMeasurementApp/Length.java
@@ -0,0 +1,55 @@
+package com.quantityMeasurementApp;
+
+import java.util.Objects;
+
+public class Length {
+
+	private final double value;
+	private final LengthUnit unit;
+
+	public enum LengthUnit {
+		FEET(1.0), INCHES(1.0 / 12.0);
+
+		private final double toFeetFactor;
+
+		LengthUnit(double toFeetFactor) {
+			this.toFeetFactor = toFeetFactor;
+		}
+
+		public double toFeet(double value) {
+			return value * toFeetFactor;
+		}
+	}
+
+	public Length(double value, LengthUnit unit) {
+		if (unit == null) {
+			throw new IllegalArgumentException("unit not null");
+		}
+		this.value = value;
+		this.unit = unit;
+	}
+
+	private double toBaseUnit() {
+		return unit.toFeet(value);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+
+		Length other = (Length) obj;
+
+		return Double.compare(this.toBaseUnit(), other.toBaseUnit()) == 0;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(toBaseUnit());
+	}
+}
---
+++ src/main/java/com/quantityMeasurementApp/QuantityMeasurementApp.java
@@ -0,0 +1,35 @@
+package com.quantityMeasurementApp;
+
+public class QuantityMeasurementApp {
+
+	public static boolean demonstrateLengthEquality(Length l1, Length l2) {
+		return l1.equals(l2);
+	}
+
+	public static void demonstrateFeetEquality() {
+		Length l1 = new Length(1.0, Length.LengthUnit.FEET);
+		Length l2 = new Length(1.0, Length.LengthUnit.FEET);
+
+		System.out.println("feet equal : " + l1.equals(l2));
+	}
+
+	public static void demonstrateInchesEquality() {
+		Length l1 = new Length(1.0, Length.LengthUnit.INCHES);
+		Length l2 = new Length(1.0, Length.LengthUnit.INCHES);
+
+		System.out.println("inches equal : " + l1.equals(l2));
+	}
+
+	public static void demonstrateFeetInchesComparison() {
+		Length l1 = new Length(1.0, Length.LengthUnit.FEET);
+		Length l2 = new Length(12.0, Length.LengthUnit.INCHES);
+
+		System.out.println("feet == inches : " + l1.equals(l2));
+	}
+
+	public static void main(String[] args) {
+		demonstrateFeetEquality();
+		demonstrateInchesEquality();
+		demonstrateFeetInchesComparison();
+	}
+}
