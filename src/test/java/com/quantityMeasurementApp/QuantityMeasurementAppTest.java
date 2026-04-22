00368c5a12a08bbc97b292450b4f3873acd0d28e
Kiran <kirankurhade-hub@users.noreply.github.com>
2026-04-22 15:31:59 +0530

    [Kiran] : Feet to Inch conversion equality check and also test with Junit.

--- src/test/java/com/quantityMeasurementApp/QuantityMeasurementAppTest.java
+++ src/test/java/com/quantityMeasurementApp/QuantityMeasurementAppTest.java
@@ -1,6 +1,8 @@
 package com.quantityMeasurementApp;
 
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import com.quantityMeasurementApp.QuantityMeasurementApp.Feet;
 
 public class QuantityMeasurementAppTest {
